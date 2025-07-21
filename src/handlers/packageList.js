6980bb46c2252e1fa61766f3d210961832339f37
Shashank <shashankj677@gmail.com>
2025-07-21 17:12:21 +0545

    completed CRUD APIs for packageList

--- src/handlers/packageList.js
+++ src/handlers/packageList.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { create, findAll, findById } from "../services/packageList.js";
+import {
+  create,
+  findAll,
+  findById,
+  update,
+  remove,
+} from "../services/packageList.js";
 
 const router = Router();
 
 router.post("/", create);
 router.get("/", findAll);
 router.get("/:id", findById);
+router.patch("/:id", update);
+router.delete("/:id", remove);
 
 export default router;
--- src/services/packageList.js
+++ src/services/packageList.js
@@ -22,4 +22,33 @@ const findById = asyncHandler(async (req, res) => {
   res.status(200).json(packageList);
 });
 
-export { create, findAll, findById };
+const update = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const packageList = await PackageList.findByIdAndUpdate(
+    id,
+    {
+      name: req.body.name,
+      ...(req.body.completed && {
+        completed: req.body.completed,
+      }),
+    },
+    { new: true }
+  );
+  if (!packageList) {
+    res.status(404).json({ message: "Package list not found" });
+    return;
+  }
+  res.status(200).json(packageList);
+});
+
+const remove = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const packageList = await PackageList.findByIdAndDelete(id);
+  if (!packageList) {
+    res.status(404).json({ message: "Package list not found" });
+    return;
+  }
+  res.status(200).json({ message: "Package list deleted" });
+});
+
+export { create, findAll, findById, update, remove };
